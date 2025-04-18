986705362531887b5fe1d7af0ef9f12b2f9fcd09
skillmuffin <bhanu.baliyar@gmail.com>
2025-04-17 22:05:21 -0700

    first commit

---
+++ app/src/main/java/com/bhanu/baliyar/scrumpilitious/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bhanu.baliyar.scrumpilitious
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bhanu.baliyar.scrumpilitious.ui.theme.ScrumpilitiousTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScrumpilitiousTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScrumpilitiousTheme {
+        Greeting("Android")
+    }
+}
